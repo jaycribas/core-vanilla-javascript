bff59e5138dd51ae20cd35e9479138a8aa747de0
Jay Cribas <feverjay@gmail.com>
2017-03-01 17:13:23 -0800

    Finished day 2 exercises

---
+++ day-2/array-methods-task4.js
@@ -0,0 +1,12 @@
+var john = {name: 'John', age: 25}
+var pete = {name: 'Pete', age: 30}
+var mary = {name: 'Mary', age: 29}
+
+var arr = [john, pete, mary]
+
+function getAverageAge(arr) {
+//       arr.reduce(function(total, currentValue), initialValue)
+  return arr.reduce((sum, user) => sum + user.age, 0) / arr.length
+}
+
+console.log(getAverageAge(arr))
---
+++ day-2/array-methods-task5.js
@@ -0,0 +1,7 @@
+function unique(arr){
+  return arr.filter((word, i, a) => a.indexOf(word) == i).length
+}
+
+var strings = ['Hare', 'Krishna', 'Hare', 'Krishna', 'Krishna', 'Krishna', 'Hare', 'Hare', ':-0']
+
+console.log(unique(strings))
