d5df40a4bcd1810ef262bff0f2d58ce2f89ecfd6
Jay Cribas <feverjay@gmail.com>
2017-03-03 12:06:46 -0800

    completed Array and Function exercises from FrontEndMasters

---
+++ day-2/noise-array.js
@@ -0,0 +1,21 @@
+var noiseArray = ['honk']
+// Using a native array method...
+noiseArray.unshift('snap')
+noiseArray.push('crash')
+console.log(noiseArray)
+
+// Using Bracket Notation...
+noiseArray[3] = 'meow'
+console.log(noiseArray)
+
+// Inspect the noiseArray
+console.log(noiseArray.length)
+console.log(noiseArray[noiseArray.length - 1])
+
+var animal = {
+  username: 'DaffyDuck',
+  tagline: 'Yippeee!',
+  noises: noiseArray
+}
+
+console.log(animal.noises)
